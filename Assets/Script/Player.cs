91fd50c1840fca9a0ce09ed45d9834138bd862c9
HakunaMytata <41043010+HakunaMytata@users.noreply.github.com>
2022-05-17 07:55:17 -0400

    1

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -27,7 +27,45 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StreamReader reader = new StreamReader(FILE_NAME);
+        content = reader.ReadToEnd();
+
+        char[] delimiterChars = { '|' };
+
+        string[] scoreSplit = content.Split(delimiterChars);
+
+        List<int> allScores = new List<int>();
+        for (int i = 0; i < scoreSplit.Length; i++)
+        {
+            Debug.Log(scoreSplit[i]);
+            if (i % 2 != 0)
+            {
+                allScores.Add(int.Parse(scoreSplit[i]));
+            }
+        }
+
+        allScores.Sort();
+
+        int highestScore = allScores[allScores.Count - 1];
+
+        Debug.Log("the highest score is " + highestScore);
+
+        for (int i = allScores.Count - 1; i >= 0; i--)
+        {
+            Debug.Log("allScores");
+
+        }
+
+        int highScore = int.Parse(scoreSplit[1]);
+        int allHighScores = 100 + highScore;
+
+        Debug.Log(allHighScores);
+
+        Debug.Log("name: " + scoreSplit[0]);
+        Debug.Log("score: " + scoreSplit[1]);
+
+        Debug.Log(content);
+        reader.Close();
 
 
 
